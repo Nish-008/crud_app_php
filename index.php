183a258ba7bd3bfde8173a536cb8ece9915dc1ca
Nish-008 <farihazaman.nishat@gmail.com>
2024-01-17 21:58:23 +0600

    form creation

--- index.php
+++ index.php
@@ -48,25 +48,42 @@
             ?>
         </tbody>
     </table>
+    <form>
     <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">ADD STUDENT</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+<div class="form-group">
+    <label for="id">ID</label>
+    <input type="text" name="id" class="form-control">
+</div>
+<div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" name="name" class="form-control">
+</div>
+<div class="form-group">
+    <label for="location">Location</label>
+    <input type="text" name="location" class="form-control">
+</div>
+<div class="form-group">
+    <label for="contact">Contact</label>
+    <input type="text" name="contact" class="form-control">
+</div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-success">ADD</button>
       </div>
     </div>
   </div>
 </div>
+</form>
 <?php include('footer.php');?>
 </body>
 </html>
