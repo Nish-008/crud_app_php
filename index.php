1dd2e642d95f45a0f2e0bf1cb332511e66e097b1
Nish-008 <farihazaman.nishat@gmail.com>
2024-01-17 13:55:34 +0600

    homepage added

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 id="main_title">CRUD APP PHP</h1>
+    <h2> ALL STUDENTS </h2>
+    <div class="container">
+    <table class="table table-hover table-bordered table striped">
+        <thead>
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Location</th>
+    <th>Contact</th>
+
+</tr>        </thead>
+        <tbody>
+<tr>
+    <td>1</td>
+    <td>Suzana</td>
+    <td>Khilgaon</td>
+    <td>0188776655</td>
+</tr>
+<tr>
+    <td>3</td>
+    <td>Tamanna</td>
+    <td>Mirpur</td>
+    <td>018879995</td>
+</tr>
+
+        </tbody>
+    </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
