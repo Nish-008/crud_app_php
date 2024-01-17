d34afec36ae4101d895d234ae49a4a866be47e12
Fariha Nishat <109890246+Nish-008@users.noreply.github.com>
2024-01-17 12:45:27 +0600

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,16 @@
+<?php
+
+define("HOSTNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "crud_op");
+
+$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
+}
+
+?>
