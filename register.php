81c9eeeb88de338cc9a4e02162f366ea2178591e
Nish-008 <farihazaman.nishat@gmail.com>
2024-01-18 19:41:23 +0600

    login page added and styled

---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" class="register-page">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box form-box">
+<header>Login </header>
+<form action="" method="post">
+    <div class="field input">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username" required>
+    </div>
+    <div class="field input">
+        <label for="password">Password</label>
+        <input type="text" name="password" id="password" required>
+    </div>
+    <div class="field">
+        <input type="submit" class="btn" name="usubmit" value="LOGIN" required>
+    </div>
+    <div class="links">
+        Don't have account? <a href="register.php">Sign Up Now</a>
+    </div>
+</form>
+        </div>
+    </div>
+</body>
+</html>
